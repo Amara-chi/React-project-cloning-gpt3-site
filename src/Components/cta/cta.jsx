a6b12a45a9e8c0600337a0340b48bb237acf1c50
Amarachi Nwachukwu <namarachi525@gmail.com>
2024-01-12 09:47:37 +0100

    Final look

---
+++ src/Components/cta/cta.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './cta.css'
+
+const cta = () => {
+  return (
+    <div className='gpt3__cta'>
+      <div className="gpt3__cta-content">
+        <p>Request Early Access</p>
+        <h3>Register today & start enjoying the endless possibilities</h3>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type='button'>Get started</button>
+      </div>
+    </div>
+  )
+}
+
+export default cta
